a210451c16f2ee7a7d29c6f343d53c4020b668e0
VexarCoding <malzahnparker@>
2025-05-29 18:10:09 -0500

    Update

--- lua/plugins/nvim-scrollbar.lua
+++ lua/plugins/nvim-scrollbar.lua
@@ -1,4 +1,26 @@
 return {
   "petertriho/nvim-scrollbar",
-  lazy = false,
+  event = "VeryLazy",
+  config = function()
+    local colors = require("tokyonight.colors").setup()
+
+    require("scrollbar").setup({
+      handle = {
+        color = colors.bg_highlight, -- scrollbar handle
+      },
+      marks = {
+        Search = { color = colors.orange },
+        Error  = { color = colors.error },
+        Warn   = { color = colors.warning },
+        Info   = { color = colors.info },
+        Hint   = { color = colors.hint },
+        Misc   = { color = colors.purple },
+      },
+    })
+
+    -- Enable optional handlers
+    require("scrollbar.handlers.gitsigns").setup()
+    require("scrollbar.handlers.search").setup()
+    require("scrollbar.handlers.diagnostic").setup()
+  end,
 }
