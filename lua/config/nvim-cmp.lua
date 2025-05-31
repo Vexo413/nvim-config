7fe68ce35c094d965dcf55b8f1af84ed11217703
VexarCoding <malzahnparker@>
2025-05-31 18:20:39 -0500

    Updated

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -1,38 +1,43 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
+local types = require('cmp.types')
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-    }),
-    sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
-    },
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  preselect = types.cmp.PreselectMode.Item,
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+  }),
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  },
+  experimental = {
+    ghost_text = true,
+  },
 })
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,9 +1,9 @@
 return {
-  "echasnovski/mini.nvim",
-  config = function()
-    require('mini.icons').setup()
-    require('mini.sessions').setup()
-    require('mini.pairs').setup()
-    -- no other modules enabled
-  end,
+  {
+    "echasnovski/mini.pairs",
+    event = "InsertEnter",
+    opts = {}
+  },
+  { "echasnovski/mini.icons" },
+  { "echasnovski/mini.sessions" }
 }
